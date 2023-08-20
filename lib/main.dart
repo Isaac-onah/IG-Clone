6b3c0e3b7e323e5570d0d59aaa06c5df6526ce76
Aiyzihk <iumeha@yahoo.com>
2023-08-20 01:40:44 +0100

    message

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-     // called again, and so nothing would appear to happen.
-      _counter++;
+     _counter++;
     });
   }
 
