19484abb4d842ba41f069aae8e0884a4c5e27348
Aiyzihk <iumeha@yahoo.com>
2023-09-10 07:45:44 +0100

    message

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -10,4 +10,13 @@ class User {
   final String bio;
   final List followers;
   final List following;
+
+  const User(
+      {required this.username,
+        required this.uid,
+        required this.photoUrl,
+        required this.email,
+        required this.bio,
+        required this.followers,
+        required this.following});
 }
