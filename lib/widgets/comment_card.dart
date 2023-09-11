1823fb657f3475ec7f87dde016b2e6d8f839a9cd
Aiyzihk <iumeha@yahoo.com>
2023-09-11 08:30:44 +0100

    message

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -17,6 +17,40 @@ class CommentCard extends StatelessWidget {
             ),
             radius: 18,
           ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: snap.data()['name'],
+                            style: const TextStyle(fontWeight: FontWeight.bold,)
+                        ),
+                        TextSpan(
+                          text: ' ${snap.data()['text']}',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      DateFormat.yMMMd().format(
+                        snap.data()['datePublished'].toDate(),
+                      ),
+                      style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w400,),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
           ])
     );
   }
