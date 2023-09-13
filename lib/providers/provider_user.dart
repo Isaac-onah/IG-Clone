d85d5abae42e0c6f69c8c8747e121bc545a16237
Aiyzihk <iumeha@yahoo.com>
2023-09-13 09:40:44 +0100

    message

--- lib/providers/provider_user.dart
+++ lib/providers/provider_user.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 class UserProvider with ChangeNotifier {
+  User? _user;
+  final AuthMethods _authMethods = AuthMethods();
 
+  User get getUser => _user!;
 }
