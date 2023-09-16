b2fe2e1aab12ed2836e67647cbc05960e9bedf53
Aiyzihk <iumeha@yahoo.com>
2023-09-16 09:55:44 +0100

    message

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -8,4 +8,11 @@ class StorageMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-}
+  // adding image to firebase storage
+  Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost) async {
+
+  }
+    // creating location to our firebase storage
+
+
+  }
