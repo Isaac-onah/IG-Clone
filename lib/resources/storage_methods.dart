c24d4e56e3c78e2c07d66dc4f2fdf738168bfa4e
Aiyzihk <iumeha@yahoo.com>
2023-09-17 08:30:44 +0100

    message

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -11,6 +11,13 @@ class StorageMethods {
     // adding image to firebase storage
     Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost) async {
       // creating location to our firebase storage
+      Reference ref =
+      _storage.ref().child(childName).child(_auth.currentUser!.uid);
+      if(isPost) {
+        String id = const Uuid().v1();
+        ref = ref.child(id);
+      }
+
       }
 
   }
