6527ff0e9cbbc2c2a979c795f321435d461dc8f6
Aiyzihk <iumeha@yahoo.com>
2023-09-21 07:40:44 +0100

    message

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,5 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
 }
