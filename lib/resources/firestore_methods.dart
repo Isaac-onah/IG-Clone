1d758fbf382087927427e3e4ae9a973d65d55b55
Aiyzihk <iumeha@yahoo.com>
2023-09-27 11:30:44 +0100

    message

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:uuid/uuid.dart';
-import 'package:instagram_clone_flutter/models/post.dart';
+import 'package:igclone/models/post.dart';
 import 'package:instagram_clone_flutter/resources/storage_methods.dart';
