b4cbf550163c421ebcb1749e558184eb80fc419e
Aiyzihk <iumeha@yahoo.com>
2023-10-01 01:30:44 +0100

    message

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -6,32 +6,33 @@ import 'package:igclone/resources/storage_methods.dart';
 
 class FireStoreMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
 // asking uid here because we dont want to make extra calls to firebase auth when we can just get from our state management
   Future<String> uploadPost(String description, Uint8List file, String uid,
       String username, String profImage) async {
-
-  String res = "Some error occurred";
-  try {
-  String photoUrl =
-  await StorageMethods().uploadImageToStorage('posts', file, true);
-  String postId = const Uuid().v1(); // creates unique id based on time
-  Post post = Post(
-  description: description,
-  uid: uid,
-  username: username,
-  likes: [],
-  postId: postId,
-  datePublished: DateTime.now(),
-  postUrl: photoUrl,
-  profImage: profImage,
-  );
-  _firestore.collection('posts').doc(postId).set(post.toJson());
-  res = "success";
-  }catch (err) {
-  res = err.toString();
+    String res = "Some error occurred";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+      String postId = const Uuid().v1(); // creates unique id based on time
+      Post post = Post(
+        description: description,
+        uid: uid,
+        username: username,
+        likes: [],
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+      );
+      _firestore.collection('posts').doc(postId).set(post.toJson());
+      res = "success";
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
   }
-  return res;
-}
+
   Future<String> likePost(String postId, String uid, List likes) async {
     String res = "Some error occurred";
     try {
@@ -40,17 +41,22 @@ class FireStoreMethods {
         _firestore.collection('posts').doc(postId).update({
           'likes': FieldValue.arrayRemove([uid])
         });
-      }else {
+      } else {
         // else we need to add uid to the likes array
         _firestore.collection('posts').doc(postId).update({
           'likes': FieldValue.arrayUnion([uid])
         });
       }
       res = 'success';
-
-    }catch (err) {
+    } catch (err) {
       res = err.toString();
     }
     return res;
+  }
+
+  // Post comment
+  Future<String> postComment(String postId, String text, String uid,
+      String name, String profilePic) async {
+
   }
 }
