099a05e91dbcf42620ffe282558edcf420ad32b8
Aiyzihk <iumeha@yahoo.com>
2023-10-01 06:30:44 +0100

    message

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -62,6 +62,20 @@ class FireStoreMethods {
       if (text.isNotEmpty) {
         // if the likes list contains the user uid, we need to remove it
         String commentId = const Uuid().v1();
+        _firestore
+            .collection('posts')
+            .doc(postId)
+            .collection('comments')
+            .doc(commentId)
+            .set({
+          'profilePic': profilePic,
+          'name': name,
+          'uid': uid,
+          'text': text,
+          'commentId': commentId,
+          'datePublished': DateTime.now(),
+        });
+        res = 'success';
       } else {
         res = "Please enter text";
       }
