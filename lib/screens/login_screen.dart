8cf485bc005504a66b31a256ac6a467bed8285a5
Aiyzihk <iumeha@yahoo.com>
2023-10-02 02:40:44 +0100

    message

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,6 +6,6 @@ import 'package:igclone/responsive/responsive_layout.dart';
 import 'package:igclone/responsive/web_screen_layout.dart';
 // import 'package:igclone/screens/signup_screen.dart';
 import 'package:igclone/utils/colors.dart';
-import 'package:instagram_clone_flutter/utils/global_variable.dart';
+import 'package:igclone/utils/global_variable.dart';
 import 'package:instagram_clone_flutter/utils/utils.dart';
 import 'package:instagram_clone_flutter/widgets/text_field_input.dart';
