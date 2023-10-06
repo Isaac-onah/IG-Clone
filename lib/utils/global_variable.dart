54574b6a0cd789b9ff45c26495eb983e6549d317
Aiyzihk <iumeha@yahoo.com>
2023-10-06 05:40:44 +0100

    message

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -4,3 +4,15 @@ import 'package:igclone/screens/add_post_screen.dart';
 import 'package:igclone/screens/feed_screen.dart';
 import 'package:igclone/screens/profile_screen.dart';
 import 'package:igclone/screens/search_screen.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
