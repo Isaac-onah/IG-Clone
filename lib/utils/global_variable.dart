fc6027c0c5a0c4b8631608043d89fa92c162f110
Aiyzihk <iumeha@yahoo.com>
2023-10-06 03:30:44 +0100

    message

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -2,3 +2,5 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:igclone/screens/add_post_screen.dart';
 import 'package:igclone/screens/feed_screen.dart';
+import 'package:igclone/screens/profile_screen.dart';
+import 'package:instagram_clone_flutter/screens/search_screen.dart';
