52fcb4667f436f48607dbf84e435b57421c0e4ce
Aiyzihk <iumeha@yahoo.com>
2023-10-07 03:30:44 +0100

    message

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:igclone/providers/user_provider.dart';
 import 'package:igclone/resources/firestore_methods.dart';
-import 'package:instagram_clone_flutter/utils/colors.dart';
+import 'package:igclone/utils/colors.dart';
 import 'package:instagram_clone_flutter/utils/utils.dart';
 import 'package:provider/provider.dart';
