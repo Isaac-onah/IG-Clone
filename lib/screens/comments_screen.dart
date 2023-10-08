4276e1d789febba8c0e76ed126fc870684ad7dd2
Aiyzihk <iumeha@yahoo.com>
2023-10-08 05:40:44 +0100

    message

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -4,6 +4,6 @@ import 'package:igclone/models/user.dart';
 import 'package:igclone/providers/user_provider.dart';
 import 'package:igclone/resources/firestore_methods.dart';
 import 'package:igclone/utils/colors.dart';
-import 'package:instagram_clone_flutter/utils/utils.dart';
+import 'package:igclone/utils/utils.dart';
 import 'package:instagram_clone_flutter/widgets/comment_card.dart';
 import 'package:provider/provider.dart';
