b10049d64477fe66259a07111abef59be7ba14a6
Aiyzihk <iumeha@yahoo.com>
2023-10-08 07:30:44 +0100

    message

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone_flutter/utils/colors.dart';
+import 'package:instagram_clone_flutter/utils/global_variable.dart';
+import 'package:instagram_clone_flutter/widgets/post_card.dart';
