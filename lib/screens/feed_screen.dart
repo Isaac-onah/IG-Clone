54b4e86ea6afc5e490606fd881a44b04d4797b8a
Aiyzihk <iumeha@yahoo.com>
2023-10-11 06:30:44 +0100

    message

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -4,3 +4,10 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:igclone/utils/colors.dart';
 import 'package:igclone/utils/global_variable.dart';
 import 'package:igclone/widgets/post_card.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
