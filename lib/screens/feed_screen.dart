28f5cdcab740a39c7d6ac3d285d09f2dd3029b74
Aiyzihk <iumeha@yahoo.com>
2023-10-11 01:45:44 +0100

    message

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:igclone/utils/colors.dart';
 import 'package:igclone/utils/global_variable.dart';
-import 'package:instagram_clone_flutter/widgets/post_card.dart';
+import 'package:igclone/widgets/post_card.dart';
