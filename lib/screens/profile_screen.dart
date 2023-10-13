8a5f5e9b7af18d5c43cd111b5e7dbcf8cd80743e
Aiyzihk <iumeha@yahoo.com>
2023-10-13 02:40:44 +0100

    message

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:igclone/resources/auth_methods.dart';
-import 'package:instagram_clone_flutter/resources/firestore_methods.dart';
+import 'package:igclone/resources/firestore_methods.dart';
 import 'package:instagram_clone_flutter/screens/login_screen.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
 import 'package:instagram_clone_flutter/utils/utils.dart';
