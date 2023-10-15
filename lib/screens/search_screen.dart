3b6b16170973bc69d6578d06e5e85dbee7fd13c2
Aiyzihk <iumeha@yahoo.com>
2023-10-15 02:40:44 +0100

    message

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,4 +2,4 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:igclone/screens/profile_screen.dart';
-import 'package:instagram_clone_flutter/utils/colors.dart';
+import 'package:igclone/utils/colors.dart';
